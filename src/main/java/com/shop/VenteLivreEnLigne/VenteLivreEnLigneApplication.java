c1b919a3fad65ad3f6ed11a9827700bb192563e3
MultiPublicFreeDevelopper <multiplatformuser22@gmail.com>
2023-05-07 15:36:27 +0100

    Initial commit

---
+++ src/main/java/com/shop/VenteLivreEnLigne/VenteLivreEnLigneApplication.java
@@ -0,0 +1,13 @@
+package com.shop.VenteLivreEnLigne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenteLivreEnLigneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenteLivreEnLigneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/VenteLivreEnLigne/VenteLivreEnLigneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.VenteLivreEnLigne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenteLivreEnLigneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
