c9c6832ec7cea6a65dcc36a65f80954cf178ac55
MultiPublicFreeDevelopper <multiplatformuser22@gmail.com>
2023-05-08 22:38:44 +0100

    Configuring the spring security !

---
+++ src/main/java/com/shop/VenteLivreEnLigne/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.shop.VenteLivreEnLigne.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+//                User.withUsername("user1").password("{noop}123").roles("USER").build(),
+//                User.withUsername("user2").password("{noop}123").roles("USER").build(),
+//                User.withUsername("user3").password("{noop}123").roles("USER").build(),
+//                User.withUsername("admin").password("{noop}123").roles("USER", "ADMIN").build()
+                User.withUsername("user1").password(passwordEncoder.encode("123")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder.encode("123")).roles("USER").build(),
+                User.withUsername("user3").password(passwordEncoder.encode("123")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("123")).roles("USER", "ADMIN").build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin();
+        httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
+        return httpSecurity.build();
+    }
+}
